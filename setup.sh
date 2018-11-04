2e803431b4bc76a6e3832c4568a8b3543687e993
Nik Bamert <bamertn@ethz.ch>
2018-11-04 16:35:35 +0100

    added gitconfig to dotfiles

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+#Setup VIM
+echo "source ~/dotfiles/vim/.vimrc" > ~/.vimrc 
+#Setup Tmux
+echo "source ~/dotfiles/tmux/.tmux.conf" > ~/.tmux.conf
+#Setup Git
+echo "[include]" > ~/.gitconfig
+echo "  path = ~/dotfiles/git/.gitconfig" > ~/.gitconfig
