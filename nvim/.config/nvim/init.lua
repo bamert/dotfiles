8272939b093e307259077984a2f7ec65802ef752
Nik Bamert <ndbamert@gmail.com>
2023-03-04 12:21:47 +0100

    update neovim config in lua, port most of former ivmscript vim config.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,11 +2,61 @@ require('plugins')
 require('remap')
 require('telescopex')
 require('lsp')
---vim.cmd('set termguicolors')
+vim.opt.belloff = 'all'
+vim.opt.errorbells = false
+vim.opt.nu = true
+vim.opt.diffopt='vertical'
+vim.opt.relativenumber = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
 vim.opt.termguicolors = true
-vim.opt.undodir = "~/.vim/nvimundodir" --Undo file version incompatible with vim
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.signcolumn = 'yes'
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
+vim.opt.expandtab=true
+vim.opt.cursorline=true
+vim.opt.timeoutlen=1000
+vim.opt.ttimeoutlen=10
+-- open new splits on right and below
+vim.opt.splitbelow=true
+vim.opt.splitright=true
+vim.opt.path:append("**") --set path+=** (include subdirs in search path)
+vim.opt.wildmenu=true
+-- No tilde in empty lines
+-- highlight! EndOfBuffer ctermbg=bg ctermfg=bg guibg=bg guifg=bg
+-- The following only works for normal buffers in neovim, but not in nerdtree
+vim.wo.fillchars='eob: '
+
+vim.g.NERDTreeShowHidden = 1
+vim.g.NERDTreeDirArrowExpandable = '+'
+vim.g.NERDTreeDirArrowCollapsible = '-'
+vim.g.NERDTreeMinimalUI=1
+
+-- statusline
+vim.opt.laststatus=2
+--vim.opt.statusline = "%1*\ %n\ %*"
+function my_statusline()
+    local branch = vim.fn.FugitiveHead()
+    return '%F %= '..branch..' %l/%L %v'
+end
+
+vim.cmd[[ set statusline=%!luaeval('my_statusline()') ]]
+-- Backups and undoing
+vim.opt.undodir = "~/.vim/nvimundodir" --Undo file version incompatible with vim
+vim.opt.undofile = true
+vim.opt.swapfile=false
+vim.opt.backup=false
+-- Search
+vim.opt.incsearch=false
+vim.opt.hls=false
+-- completion
+vim.opt.completeopt="menu,menuone"
+-- folds
+vim.opt.foldenable=false
+-- I think the following doesnt work yet
 vim.cmd("highlight clear SignColumn")
+
 
 
--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -1,5 +1,3 @@
-print("Hello from remaps")
---
 vim.keymap.set("n", "<C-n>", "<cmd>NERDTreeToggle<CR>")
 vim.keymap.set("n", "<leader>nf", "<cmd>NERDTreeFind<CR>")
 vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>", {})
@@ -7,3 +5,14 @@ vim.keymap.set("n", "<leader>mt", "<cmd>Git mergetool<cr>", {})
 vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<cr>", {})
 vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<cr>", {})
 vim.keymap.set("n", "<leader>yf", "<cmd>%y+<cr>",{})
+--Remap window navigation
+-- Credit: https://robots.thoughtbot.com/seamlessly-navigate-vim-and-tmux-splits
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
+--spelling
+vim.keymap.set("", "<leader>ll", "<cmd>setlocal spell! spelllang=en_us<CR>")
+
+-- other bindings
+vim.keymap.set("", "<leader>mm", "<cmd>silent make<cr>")
