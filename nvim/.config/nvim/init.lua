100d55771124a31e61dab2b853a1b26544f4f51f
Nik Bamert <ndbamert@gmail.com>
2023-03-17 08:25:26 +0100

    keep terminal in vimtest open by default

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -70,4 +70,6 @@ vim.cmd("colorscheme gruvbox")
 vim.cmd("highlight clear SignColumn")
 
 
-
+-- leave vim test window in normal mode (don't quit on keystroke)
+vim.cmd("let g:test#neovim#start_normal = 1") -- If using neovim strategy
+vim.cmd("let g:test#basic#start_normal = 1") -- If using basic strategy
