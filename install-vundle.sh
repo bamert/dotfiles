5d2a05735a939cce59b6f3fdbe523e9da2d91e20
Nik Bamert <ndbamert@gmail.com>
2023-04-20 09:11:58 +0200

    cleanup

--- install-vundle.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if !  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && [ ! -d 'Vundle' ] ; then
-  echo "Vundle clone skipped, directory already exists"
-fi
-
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# cd to dotfiles dir
-cd "$(dirname "${BASH_SOURCE[0]}")"
-stow git vim nvim tmux alacritty bash
